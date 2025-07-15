e72b956198dc00fe49e8a84f500d8f1ddbc3300d
Lucia Alzugaray <lucia.alzugaray24@gmail.com>
2025-07-15 20:51:00 -0300

    feat(auth): adjuntar User/Admin a la request tras validación JWT
    - Renombra verifyAuth0 a jwtCheck
    - Corrige la URL del issuer
    - Importa modelos User y Admin
    - Añade attachUser para cargar usuario en req
    - Exporta jwtCheck + attachUser

--- middlewares/verifyAuth0.js
+++ middlewares/verifyAuth0.js
@@ -1,7 +1,9 @@
 import { expressjwt } from "express-jwt";
 import jwksRsa from "jwks-rsa";
+import User from "../models/User.js";
+import Admin from "../models/Admin.js";
 
-const verifyAuth0 = expressjwt({
+const jwtCheck = expressjwt({
   secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
@@ -9,8 +11,31 @@ const verifyAuth0 = expressjwt({
     jwksUri: `https://dev-d82ap42lb6n7381y.us.auth0.com/.well-known/jwks.json`
   }),
   audience: "https://delatte.api",
-  issuer: "https://dev-d82ap42lb6n7381y.us.auth0.com/",  
+  issuer: "https://dev-d82ap42lb6n7381y.us.auth0.com/",
   algorithms: ["RS256"]
 });
 
-export default verifyAuth0;
+async function attachUser(req, res, next) {
+  try {
+    const sub = req.auth.sub;
+    let user = await User.findOne({ auth0Id: sub });
+
+    if (!user) {
+      const adminDoc = await Admin.findOne({ auth0Id: sub });
+      if (adminDoc) {
+        user = await User.findById(adminDoc.userId);
+      }
+    }
+
+    if (!user) {
+      return res.status(401).json({ error: "Usuario no encontrado" });
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
+export default [jwtCheck, attachUser];
